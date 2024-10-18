8b1b86cc9d0b92a86aa220572d7177f4d1332af1
Squad de UX Sebrae <squaddeuxsebrae@gmail.com>
2024-10-18 15:55:02 -0300

    Carga inicial
    Carga inicial

---
+++ js/theme.js
@@ -0,0 +1,56 @@
+// função que preenche o form
+function generateEmail() {
+
+  var email = document.querySelector('.conteudoResultado');
+
+  //inputs
+  var merchantName = document.querySelector('.InputName');
+  var merchantCargo = document.querySelector('.InputCargo');
+  var merchantUnidade = document.querySelector('.InputUnidade');
+  var merchantNucleo = document.querySelector('.InputNucleo');
+  var merchantTelefone = document.querySelector('.InputTelefone');
+  var merchantEmail = document.querySelector('.InputEmail');
+
+  //outputs
+  var paragraphName = document.querySelector('.insertName');
+  var paragraphCargo = document.querySelector('.insertCargo');
+  var paragraphUnidade = document.querySelector('.insertUnidade');
+  var paragraphNucleo = document.querySelector('.insertNucleo');
+  var paragraphTelefone = document.querySelector('.insertTelefone');
+  var paragraphEmail = document.querySelector('.insertEmail');
+
+  // preenche
+  paragraphName.textContent = merchantName.value;
+  paragraphCargo.textContent = merchantCargo.value;
+  paragraphUnidade.textContent = merchantUnidade.value;
+  paragraphNucleo.textContent = merchantNucleo.value;
+  paragraphTelefone.textContent = merchantTelefone.value;
+  paragraphEmail.textContent = merchantEmail.value;
+}
+
+// Função para copiar o conteúdo da tabela com imagens e texto
+document.getElementById('copiarConteudo').addEventListener('click', function () {
+  const conteudo = document.getElementById('conteudoResultado').outerHTML;
+  const blob = new Blob([conteudo], { type: 'text/html' });
+
+  const item = new ClipboardItem({ "text/html": blob });
+  navigator.clipboard.write([item]).then(function () {
+    alert('Conteúdo copiado com sucesso!');
+  }).catch(function (error) {
+    console.error('Erro ao copiar o conteúdo: ', error);
+  });
+});
+
+// validação
+$(function () {
+  $("#gerarassinatura").on("click", function (e) {
+      var form = $("#cadastroForm")[0];
+      var isValid = form.checkValidity();
+      if (!isValid) {
+          e.preventDefault();
+          e.stopPropagation();
+      }
+      form.classList.add('was-validated');
+      return false; // For testing only to stay on this page
+  });
+});
